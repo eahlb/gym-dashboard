751c471eb9896afd1be43e965a080ce292894815
Erik Ahlberg <eahlberg12@gmail.com>
2021-08-20 09:32:23 +0200

    Make pi-startup executable

--- pi-startup.sh
+++ pi-startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+git pull
 docker pull eahlberg12/gym-dashboard:latest
-rm docker-compose.yml
-wget https://raw.githubusercontent.com/eahlb/gym-dashboard/master/docker-compose.yml
 docker-compose down
 docker-compose up -d
 chromium-browser --start-fullscreen http://localhost:8080 &
