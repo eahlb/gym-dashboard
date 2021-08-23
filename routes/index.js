f0f6a4e802f607b0d20b1d2939efb51bda25d220
Erik Ahlberg <erik.ahlberg@knowit.se>
2021-08-23 11:15:39 +0200

    Add timer view

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Dashboard' });
 });
+
+router.get('/timer', function (req, res, next) {
+  res.render('timer', { title: 'Timer' })
+})
 
 module.exports = router;
