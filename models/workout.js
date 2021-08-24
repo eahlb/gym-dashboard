beafebe3c608e30c86ef9fca0386bce9fe4e52f6
Erik Ahlberg <erik.ahlberg@knowit.se>
2021-08-24 14:13:19 +0200

    Extend workout model

--- models/workout.js
+++ models/workout.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const setSchema = new Schema({
+    reps: { type: Number, required: true },
+    load: { type: String, required: true },
+    time: { type: Number, required: true },
+});
+
+const excerciseSchema = new Schema({
+    name: { type: String, required: true },
+    sets: [setSchema],
+});
+
 const workoutSchema = new Schema({
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    excercises: [excerciseSchema],
 });
 
 module.exports = mongoose.model('Workout', workoutSchema);
