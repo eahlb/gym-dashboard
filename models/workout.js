848a2b0a32ea4512eb50bed9fe0fa6854ee39283
Erik Ahlberg <erik.ahlberg@knowit.se>
2021-08-24 13:56:18 +0200

    Add simple workout model

---
+++ models/workout.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    name: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Workout', workoutSchema);
