d0e52b9cd44fbdfc64857d8ab6ec7e7a3e5cd4e7
Erik Ahlberg <erik.ahlberg@knowit.se>
2021-08-25 15:14:59 +0200

    Add workout POST endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,33 @@ const router = express.Router();
 const repo = require('../lib/workoutRepository');
 
 /* GET pages. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   res.render('index', { title: 'Dashboard' });
 });
 
-router.get('/timer', function (req, res, next) {
+router.get('/timer', function (req, res) {
   res.render('timer', { title: 'Timer' });
 });
 
-router.get('/create', function (req, res, next) {
+router.get('/create', function (req, res) {
   res.render('create', { title: 'Create Workout' });
 });
 
 /* WORKOUT */
-router.post('/workout', function (req, res, next) {
-  repo.saveWorkout(req.body);
-  res.redirect('/')
+router.post('/workout', function (req, res) {
+  repo.saveWorkout(req.body)
+    .then((value) => res.send(value))
+    .catch((reason) => res.status(500).json({ error: reason }));
 });
+
+router.get('/workout', function (req, res) {
+  // TODO: get all workouts
+  res.send('OK GET')
+});
+
+router.get('/workout/:name', function (req, res) {
+  // TODO: get workouts by name
+  res.send('OK GET')
+})
 
 module.exports = router;
