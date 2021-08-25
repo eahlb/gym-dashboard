55483ff28327439abf72088f8b37f1bebf501adf
Erik Ahlberg <erik.ahlberg@knowit.se>
2021-08-25 15:38:59 +0200

    Edit startup script

--- pi-startup.sh
+++ pi-startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+docker-compose down
 git pull
 docker pull eahlberg12/gym-dashboard:latest
 docker-compose down
-docker-compose up -d
-chromium-browser --start-fullscreen http://localhost:8080 &
-exit 0
+docker-compose up
