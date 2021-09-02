44f3433669a1e2b8d62e7004a87d547669a64027
Erik Ahlberg <erik.ahlberg@knowit.se>
2021-09-02 13:59:15 +0200

    Add config

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const server = require('./server')();
 const config = require('./config/config.json');
 
-// TODO: Move config
-config.env = 'development'
-config.port = '3000'
-
 server.create(config);
 server.start();
