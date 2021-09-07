b366142c6e2a43a0507da547d8b9c7bcec332888
Erik Ahlberg <erik.ahlberg@knowit.se>
2021-09-07 16:09:31 +0200

    Add programs to index view

--- server/routes/views.js
+++ server/routes/views.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const repo = require('../database/repository');
 
 router.get('/', function (req, res) {
-  repo.findWorkouts({ status: 'PENDING' })
-    .then((value) => res.render('index', { title: 'Dashboard', workouts: value }));
+  repo.listPrograms()
+    .then((value) => res.render('index', { title: 'Dashboard', programs: value }));
 });
 
 router.get('/timer', function (req, res) {
@@ -13,6 +13,12 @@ router.get('/timer', function (req, res) {
 
 router.get('/workout/:id', function (req, res) {
   repo.findWorkout(req.params.id)
+    .then((value) => res.render('workout', { data: value }))
+    .catch((reason) => res.render('error', { error: reason }));
+});
+
+router.get('/program/:programId/workout/:workoutId', function (req, res) {
+  repo.findWorkout(req.params)
     .then((value) => res.render('workout', { data: value }))
     .catch((reason) => res.render('error', { error: reason }));
 });
