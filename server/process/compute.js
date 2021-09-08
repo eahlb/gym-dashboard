028bc54ee1becded62e13d2db26e90d235ca1144
Erik Ahlberg <erik.ahlberg@knowit.se>
2021-09-08 10:11:50 +0200

    Replace workout variables

--- server/process/compute.js
+++ server/process/compute.js
@@ -1,12 +1,31 @@
+const replace = (str, input) => {
+    let result = str;
+    input.forEach(i => {
+        console.log(result)
+        result = result.replace(`%${i.name}%`, i.value);
+    });
+    return result;
+};
+
 const compute = (workout) => {
     const excercises = [];
     const input = workout.input;
     workout.excercises.forEach((ex) => {
-        // TODO: Compute excercises
-        excercises.push(ex);
+        const result = {
+            excerciseName: replace(ex.excerciseName, input),
+            sets: [],
+        };
+        ex.sets.forEach(set => {
+            // TODO: Compute load.  
+            result.sets.push({
+                reps: replace(set.reps, input),
+                load: replace(set.load, input),
+            })
+        })
+        excercises.push(result);
     });
     workout.excercises = excercises;
     return workout;
-}
+};
 
 module.exports = compute;
