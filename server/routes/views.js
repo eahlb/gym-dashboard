1de08bd13acbd616551d2341cd05fed0962c1ccd
Erik Ahlberg <erik.ahlberg@knowit.se>
2021-09-08 16:48:59 +0200

    Show only next pending workout on index page

--- server/routes/views.js
+++ server/routes/views.js
@@ -5,7 +5,18 @@ const compute = require('../process/compute');
 
 router.get('/', function (req, res) {
   repo.listPrograms()
-    .then((value) => res.render('index', { title: 'Dashboard', programs: value }));
+    .then((value) => res.render('index', {
+      title: 'Dashboard',
+      programs: value
+        .filter(p => p.workouts.find(w => w.status == 'PENDING'))
+        .map(p => {
+          return {
+            _id: p._id,
+            programName: p.programName,
+            workout: p.workouts.find(w => w.status == 'PENDING'),
+          }
+        }),
+    }));
 });
 
 router.get('/timer', function (req, res) {
